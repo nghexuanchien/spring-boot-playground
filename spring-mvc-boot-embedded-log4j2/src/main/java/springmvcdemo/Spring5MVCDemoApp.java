0ee6f130dd125bfd8f25ab55e6863ad8320c5e58
chiennghe <chienptit@gmail.com>
2019-05-27 23:33:19 +0700

    spring-mvc-boot-embedded-log4j2

---
+++ spring-mvc-boot-embedded-log4j2/src/main/java/springmvcdemo/Spring5MVCDemoApp.java
@@ -0,0 +1,14 @@
+package springmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chiennghe
+ */
+@SpringBootApplication
+public class Spring5MVCDemoApp {
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5MVCDemoApp.class, args);
+	}
+}
